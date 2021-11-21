bf3fdc3f845c930a30aff22ad4af39b77b940816
DesireeJoy <desireebradish@gmail.com>
2021-11-21 11:45:00 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/DesireeJoy_Portfolio/precache-manifest.be86d87aad12b12180d3bfd1926aeeeb.js"
+  "/DesireeJoy_Portfolio/precache-manifest.6e0fae95b165de7f097ba70774c74192.js"
 );
 
 self.addEventListener('message', (event) => {
