a344bdea5887185a51a1da303721b2f7c8caa85d
DesireeJoy <desireebradish@gmail.com>
2022-01-10 13:51:30 -0500

    Added video link for portfolio

--- src/constants/projectList.js
+++ src/constants/projectList.js
@@ -3,7 +3,7 @@ const projectList = [
     title: "News Explorer",
     image: "imageOne",
     desc: "React Frontend, Express Backend. Hosted on AWS utilizing an Nginx reverse proxy.",
-    live: "https://djbnews.students.nomoreparties.site/",
+    live: "https://djbnews.students.nomoreparties.sbs/",
     link:
       "https://github.com/DesireeJoy/news-explorer-full",
       alt: "Project image for a full stack news explorer app",
@@ -16,17 +16,17 @@ const projectList = [
     live: "https://desireejoy.github.io/SaveAFox-temp/",
     link:
       "https://github.com/DesireeJoy/SaveAFox",
-        alt: "Project image for the react SPA for the Save A Fox Rescue's book series",
+        alt: "Project image for the react SPA for the Save A Fox Rescue's book series. Video will come as soon as development is complete.",
          video: "",
   },
    {
     title: "Portfolio",
     image: "imageThree",
-        desc: "My coding portfolio site. A responsive React SPA with an original design. ",
+        desc: "My coding portfolio site. A responsive React SPA with an original design.",
     live: "",
     link:
       "http://www.DesireeJoy.com",
-    video: ""
+    video: "https://www.youtube.com/watch?v=_MEzTHINnhg&t=1s"
   }, 
   
   
